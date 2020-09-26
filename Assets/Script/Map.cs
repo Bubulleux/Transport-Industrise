3f68ffcf0a17d833320b9419c441890ac165ed29
Bubulle <bubulle-lol@yahoo.fr>
2020-09-26 12:01:50 +0200

    road

---
+++ Assets/Script/Map.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public static Map instence;
+    public GameObject roadPrefab;
+    public Parcel[,] parcels;
+
+    private void Awake()
+    {
+        instence = this;
+    }
+
+    void Start()
+    {
+        parcels = new Parcel[100, 100];
+    }
+    
+    void Update()
+    {
+        
+    }
+    public bool AddRoad(Vector2Int pos)
+    {
+        if (parcels[pos.x, pos.y] == null)
+        {
+            Instantiate(roadPrefab, new Vector3(pos.x, 0f, pos.y), Quaternion.identity, transform);
+            parcels[pos.x, pos.y] = new Road();
+            Debug.LogFormat("Road Make in {0}", pos);
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Parcel
+{
+    Vector2Int pos;
+}
---
+++ Assets/Script/PlayerControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Camera cam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Map.instence.AddRoad(Vec3ToVec2Int(GetMoussePos()));
+        }
+    }
+
+    public Vector3 GetMoussePos()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+
+    public static Vector2Int Vec3ToVec2Int(Vector3 vec)
+    {
+        return new Vector2Int(Mathf.FloorToInt(vec.x), Mathf.FloorToInt(vec.z));
+    }
+}
